eab1540b2ebdb0ba4d357c107e50ed7959684cd5
renalidi <sumitomo.andi@gmail.com>
2023-12-10 22:45:16 +0700

    add:vercel.json

--- functions/src/config/db/db.connection.ts
+++ functions/src/config/db/db.connection.ts
@@ -1,6 +1,8 @@
 import { PrismaClient } from '@prisma/client';
 
-export const prisma = new PrismaClient();
+export const prisma = new PrismaClient({
+  log: ["info"]
+});
 
 const connectDB = async ()=> {
   return await prisma.$connect();
