d2551e4370fa3500299cb31bad8c0e8f8c4f7efe
renalidi <sumitomo.andi@gmail.com>
2023-12-11 20:39:09 +0700

    feat: add delete foodConsumed

--- functions/src/routes/foodConsumedRoutes.ts
+++ functions/src/routes/foodConsumedRoutes.ts
@@ -1,10 +1,11 @@
 import express from 'express'
-import { createDailyFoodConsumed, getDailyFoodConsumed, updateDailyFoodConsumed } from '../controllers/foodConsumedController'
+import { createDailyFoodConsumed, deleteDailyFoodConsumed, getDailyFoodConsumed, updateDailyFoodConsumed } from '../controllers/foodConsumedController'
 
 const foodConsumedRoutes = express.Router()
 
 foodConsumedRoutes.post('/consume', createDailyFoodConsumed)
 foodConsumedRoutes.get('/consume', getDailyFoodConsumed)
 foodConsumedRoutes.put('/consume/:uniqueId', updateDailyFoodConsumed)
+foodConsumedRoutes.delete('/consume/:uniqueId', deleteDailyFoodConsumed)
 
 export default foodConsumedRoutes
