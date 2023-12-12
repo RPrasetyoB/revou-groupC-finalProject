56e758f8cde027a5b516d6366845f36408cf0ea9
renalidi <sumitomo.andi@gmail.com>
2023-12-13 02:20:40 +0700

    try develop

--- functions/dist/routes/waterRoutes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// import { getAllTodo, createTodo, updateTodo, deleteTodo } from '../controllers/waterController'
-// const todoRoutes = express.Router()
-// todoRoutes.get('/todos', getAllTodo)
-// todoRoutes.post('/todos', createTodo)
-// todoRoutes.put('/todos/:id', updateTodo)
-// todoRoutes.delete('/todos/:id', deleteTodo)
-// export default todoRoutes
