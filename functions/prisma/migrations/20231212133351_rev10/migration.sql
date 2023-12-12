9b93ae470eaace2774081bec387b5eef6940c770
renalidi <sumitomo.andi@gmail.com>
2023-12-12 20:34:47 +0700

    rebuild DB structure

--- functions/prisma/migrations/20231212133351_rev10/migration.sql
+++ functions/prisma/migrations/20231212133351_rev10/migration.sql
@@ -13,6 +13,7 @@
   - You are about to drop the column `water_actual` on the `Water` table. All the data in the column will be lost.
   - You are about to drop the `Food` table. If the table is not empty, all the data it contains will be lost.
   - A unique constraint covering the columns `[foodName]` on the table `FoodList` will be added. If there are existing duplicate values, this will fail.
+  - A unique constraint covering the columns `[googleId]` on the table `User` will be added. If there are existing duplicate values, this will fail.
   - Added the required column `foodName` to the `FoodList` table without a default value. This is not possible if the table is not empty.
   - Added the required column `medicationActual` to the `Meditation` table without a default value. This is not possible if the table is not empty.
   - Added the required column `currentMood` to the `Mood` table without a default value. This is not possible if the table is not empty.
@@ -22,9 +23,6 @@
   - Added the required column `waterActual` to the `Water` table without a default value. This is not possible if the table is not empty.
 
 */
--- CreateEnum
-CREATE TYPE "Category" AS ENUM ('skinny', 'normal', 'over');
-
 -- AlterEnum
 BEGIN;
 CREATE TYPE "Activeness_new" AS ENUM ('light', 'moderate', 'active');
@@ -54,6 +52,9 @@ ALTER TABLE "Mood" DROP COLUMN "current_mood",
 ADD COLUMN     "currentMood" "Todaymood" NOT NULL;
 
 -- AlterTable
+ALTER TABLE "Report" ADD COLUMN     "category" DOUBLE PRECISION;
+
+-- AlterTable
 ALTER TABLE "Sleep" DROP COLUMN "sleep_actual",
 DROP COLUMN "sleep_end",
 DROP COLUMN "sleep_start",
@@ -67,7 +68,7 @@ ADD COLUMN     "stepsActual" INTEGER NOT NULL;
 
 -- AlterTable
 ALTER TABLE "User" DROP COLUMN "fullName",
-ADD COLUMN     "category" "Category",
+ADD COLUMN     "googleId" TEXT,
 ADD COLUMN     "nickname" TEXT;
 
 -- AlterTable
@@ -105,6 +106,9 @@ CREATE TABLE "FoodConsumed" (
 
 -- CreateIndex
 CREATE UNIQUE INDEX "FoodList_foodName_key" ON "FoodList"("foodName");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_googleId_key" ON "User"("googleId");
 
 -- AddForeignKey
 ALTER TABLE "Calories" ADD CONSTRAINT "Calories_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
