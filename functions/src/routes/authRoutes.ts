9bd9514b929689f25f8ac50c402be90dae0b91ae
renalidi <sumitomo.andi@gmail.com>
2023-12-13 22:55:07 +0700

    feat: email verification

--- functions/src/routes/authRoutes.ts
+++ functions/src/routes/authRoutes.ts
@@ -1,12 +1,12 @@
 import express from 'express'
-import { login, logoutUser, regUser } from '../controllers/userController'
+import { emailVerification, login, regUser } from '../controllers/userController'
 import passport from '../config/auth/googleAuth'
 
 const authRoutes = express.Router()
 
 authRoutes.post('/auth/register', regUser)
 authRoutes.post('/auth/login', login)
-authRoutes.post('/logout', logoutUser)
+authRoutes.get('/verify', emailVerification)
 authRoutes.get('/auth/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
 authRoutes.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/login' }),(req, res) => { res.redirect('/')});
 
