da3c239b7edd8ae9ce77d9ab6ba5fb6973d91e05
renalidi <sumitomo.andi@gmail.com>
2023-12-17 15:01:13 +0700

    revise DB structure

--- functions/src/utils/index.d.ts
+++ functions/src/utils/index.d.ts
@@ -10,43 +10,43 @@ interface InputFood {
     foodName: string;
   }
   
-  interface MultipleFoodInput {
-    foodNames: string[];
-  }
-  
-  interface FoodListEntry {
-    foodName: string;
-    calories: number;
-  }
-  
-  interface RecordType {
-    id: number;
-  }
-  
-  interface EntryType {
-    foodName: string;
-    calories: number;
-  }
-  
-  type FoodInput = InputFood | MultipleFoodInput;
+interface MultipleFoodInput {
+  foodNames: string[];
+}
 
-  interface LoginInput {
-    username: string;
-    password: string;
-  }
-  
-  interface RegisterInput {
-    username: string;
-    email: string;
-    password: string;
-    verificationToken: string;
-  }
-  interface UpdateInput {
-    nickname: string;
-    weight: string;
-    height: string;
-    gender: string;
-    age: number;
-    activeness: string;
-    category: string;
-  }
+interface FoodListEntry {
+  foodName: string;
+  calories: number;
+}
+
+interface RecordType {
+  id: number;
+}
+
+interface EntryType {
+  foodName: string;
+  calories: number;
+}
+
+type FoodInput = InputFood | MultipleFoodInput;
+
+interface LoginInput {
+  username: string;
+  password: string;
+}
+
+interface RegisterInput {
+  username: string;
+  email: string;
+  password: string;
+  verificationToken: string;
+}
+interface UpdateInput {
+  nickname: string;
+  weight: string;
+  height: string;
+  gender: string;
+  age: number;
+  activeness: string;
+  category: string;
+}
