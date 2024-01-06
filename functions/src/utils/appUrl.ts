a0913fa4d6d6ce46895f1d8332a95524854de8f3
RPrasetyoB <rpbasuki.dev@gmail.com>
2024-01-06 15:48:53 +0700

    fix: deployment

--- functions/src/utils/appUrl.ts
+++ functions/src/utils/appUrl.ts
@@ -1,6 +1,6 @@
 //---- local server ----
 // const BE_URL = 'http://localhost:4000'
-// const FE_URL = 'http://localhost:5173'
+const FE_URL = 'http://localhost:5173'
 
 //---- dev server ----
 const BE_URL = 'https://group-c-project.onrender.com'
@@ -8,6 +8,6 @@ const BE_URL = 'https://group-c-project.onrender.com'
 
 //---- prod server ----
 // const BE_URL = 'https://us-central1-final-project-16787.cloudfunctions.net/grupc_project'
-const FE_URL = 'https://helena-health-tracker.web.app'
+// const FE_URL = 'https://helena-health-tracker.web.app'
 
 export {BE_URL, FE_URL}
